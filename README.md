49ee375efe8ab09a3305fcbfcb4e522c8e7f1ef4
Hadi Swais <hadi.swais@gmail.com>
2021-01-01 17:47:54 +0300

    fix bugs and add read me

---
+++ README.md
@@ -0,0 +1,9 @@
+### Installation
+
+1.  Clone or  Download the latest version
+2.  Create a new Database and change config file
+3.  npm i 
+4.  npx sequelize-cli db:migrate
+5.  npx sequelize-cli db:seed:all
+6.  npm test
+
--- migrations/20201230210442-create-post.js
+++ migrations/20201230210442-create-post.js
@@ -1,34 +1,37 @@
 'use strict';
-export async function up(queryInterface, Sequelize) {
-  await queryInterface.createTable('Posts', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    title: {
-      allowNull: false,
-      type: Sequelize.STRING
-    },
-    body: {
-      allowNull: false,
-      type: Sequelize.TEXT
-    },
-    userId: {
-      allowNull: false,
-      type: Sequelize.INTEGER
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    }
-  });
-}
-export async function down(queryInterface, Sequelize) {
-  await queryInterface.dropTable('Posts');
-}
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      body: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Posts');
+  }
+};
